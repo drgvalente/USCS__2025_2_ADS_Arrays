fdd98aa40c46a923bfbeb86163b9455dcedab64c
Adriano FV <54666006+drgvalente@users.noreply.github.com>
2025-09-17 11:39:27 -0300

    fim da aula 17-09

---
+++ script.js
@@ -0,0 +1,24 @@
+let arr1 = [1, 2, 3, 4, 5];
+console.log(arr1[2]);
+arr1[2] = 10;
+console.log(arr1[2]);
+
+let matr1 = [
+    [1, 2, 3],
+    [4, 5, 6]
+];
+
+console.log(matr1[1][0]);
+console.log("Iterando Arrays");
+
+for(let i = 0; i < arr1.length; i++)
+{
+    console.log(arr1[i]);
+}
+
+
+let matr3 = [
+    [[1, 2], [3, 4], [5, 6]],
+    [[7, 8], [9, 0], [1, 3]],
+    [[2, 4], [5, 7], [6, 8]]
+];
